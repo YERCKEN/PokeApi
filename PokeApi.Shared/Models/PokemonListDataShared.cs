e53f443b77ad5973689c54afc284b612b9cccb63
EDWAR GONZALEZ <yercken30@gmail.com>
2025-02-07 14:13:47 -0500

    + Nuevo proyecto para Tener los modeos  e interfaces compartidos

---
+++ PokeApi.Shared/Models/PokemonListDataShared.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.Shared.Models
+{
+
+    // lISTA DE POKEMONS
+    public class PokemonList
+    {
+        public List<PokemonListInfo> Results { get; set; } = new List<PokemonListInfo>();
+    }
+
+
+    // INFO DEL POKEMON POKEMON (nombre y url)
+    public class PokemonListInfo
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+    }
+
+}
