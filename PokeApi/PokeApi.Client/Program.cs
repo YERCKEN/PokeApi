e017607b4fc2c62a163b49be56c8c5b987f7bd27
EDWAR GONZALEZ <yercken30@gmail.com>
2025-02-10 01:32:28 -0500

    - Obteniendo los datos desde los EndPoints, en lugar de las interfaces

--- PokeApi/PokeApi.Client/Program.cs
+++ PokeApi/PokeApi.Client/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+// Añadimos el servicio de del client
+using PokeApi.Client.Services;
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Registrar el servicio de la API
+builder.Services.AddScoped<PokeApiClientService>();
+
 
 await builder.Build().RunAsync();
---
+++ PokeApi/PokeApi.Client/Services/PokeApiClientService.cs
@@ -0,0 +1,66 @@
+﻿using PokeApi.Shared.Models;
+using RestSharp;
+using System.Text.Json;
+
+namespace PokeApi.Client.Services
+{
+    public class PokeApiClientService
+    {
+        private readonly RestClient _client;
+
+        public PokeApiClientService(string baseUrl)
+        {
+            _client = new RestClient(baseUrl);
+        }
+
+
+
+        // Consumir el listado de pokémones y devolverlo como lista
+        public async Task<List<PokemonListInfo>> GetPokemonListAsync()
+        {
+            Console.WriteLine("Obteniendo lista de pokémones desde el API en SERVER...");
+            var request = new RestRequest("api/pokemon/list", Method.Get);
+
+            // Realizar la petición al servidor
+            var response = await _client.ExecuteAsync(request);
+
+            // Verificar si la petición fue exitosa y si la respuesta no es nula
+            if (response.IsSuccessful && response.Content != null)
+            {
+                // Deserializar la respuesta JSON a una lista de objetos PokemonListInfo
+                var pokemonList = JsonSerializer.Deserialize<List<PokemonListInfo>>(response.Content, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return pokemonList ?? new List<PokemonListInfo>();
+            }
+
+            return new List<PokemonListInfo>(); // Retornar lista vacía en caso de error
+        }
+
+
+        // Obtener detalles de un Pokémon por nombre
+        public async Task<PokemonCharacteristics?> GetPokemonByNameAsync(string name)
+        {
+            Console.WriteLine("Obteniendo Caracteristicas del pokemon desde el API en SERVER...");
+
+            var request = new RestRequest($"api/pokemon/{name}", Method.Get);
+            var response = await _client.ExecuteAsync(request);
+
+            if (response.IsSuccessful && response.Content != null)
+            {
+                var pokemon = JsonSerializer.Deserialize<PokemonCharacteristics>(response.Content, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return pokemon;
+            }
+
+            return null;
+        }
+
+
+    }
+}
